2205450aaebae3adaaf685a3fa0715965077ee7c
ayueyang <ayueyang@users.noreply.github.com>
2026-06-24 07:09:45 +0800

    chore: change default serverHost from 192.168.1.100 to 127.0.0.1
    Original author hardcoded their LAN IP as default; use localhost for better out-of-box experience

--- node-proxy/src/config.js
+++ node-proxy/src/config.js
@@ -90,7 +90,7 @@ if (!fs.existsSync(getConfPath())) {
 }
 // 从环境变量上读取配置信息，docker首次启动时候可以直接进行配置
 const serverAddr = process.env.ALIST_HOST
-let serverHost = '192.168.1.100'
+let serverHost = '127.0.0.1'
 let serverPort = 5244
 if (serverAddr && serverAddr.indexOf(':') > 6) {
   serverHost = serverAddr.split(':')[0]
